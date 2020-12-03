3e5981ae790fd68c2893f410da3afe16729a517b
ting29 <1837503790@qq.com>
2020-12-03 11:30:17 +0800

    test

--- src/demo01.java
+++ src/demo01.java
@@ -3,5 +3,6 @@ public class demo01 {
         System.out.println("呼叫总部，呼叫总部，收到请回答！！！");
         System.out.println("我是编号洞拐洞拐");
         System.out.println("更新了吗");
+        System.out.println("更新了");
     }
 }
